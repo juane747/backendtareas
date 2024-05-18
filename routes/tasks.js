7ee3adb7eeae3e79d6bda8990d8ae276e55f96c5
juane747 <emifive67@gmail.com>
2024-05-17 22:30:13 -0600

    Tarea semana 5 completa

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,40 +1,70 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+// abrimos conexion a bd como se hizo en goals
+var connection = mysql.createConnection({
+    host:'localhost',
+    user:'usuarioreact',
+    password:'clave',
+    database: 'desarrolloweb'
+  });
+
+//creamos la conexion
+  connection.connect(function(err){// si hay error
+    if(err){
+      console.error('error conecting '+err.stack);// imprimimos el error en consola
+      return;
+    }
+    console.log('Connected as id'+connection.threadId);
+  })
+
 
 let tasks =[];
 // definimos usar la ruta creada en app.js
 //La funcion definida con req, los datos requeridos, res la respuesta y netx en caso de usar middleware
 router.get('/getTasks', function(req, res, next){
-// con lo que estamos creando retornara un Json, antes de inicial 
-    res.status(200).json(tasks);
+    let queryGetTasks = 'SELECT * FROM tasks';
+    connection.query(queryGetTasks, function(err, results, filds){
+        if(err){
+            res.status(500).json(err);
+        }else{
+          res.status(200).json(results);
+        }
+      })
 })
 
 router.post('/addTask', function(req, res, next){
-    // aca se agrega un indice usando la fecha actual y un numero random
-    let timestamp = Date.now() + Math.random();
-    // evaluamos que exita el body y los parametos que definimos con la comunicacion de postman
-    if(req.body && req.body.name && req.body.description && req.body.dueDate){
-        req.body.id = timestamp.toString;
-        tasks.push(req.body);
-        res.status(200).json(tasks);//agregamos status 200 para indicar que todo va bien
-    }else{
-        res.status(400).json({error:"no se esta enviando parametro"});//aca enviamos un error definido ademas del status 400
-    }
+   if(req.body && req.body.name && req.body.description && req.body.dueDate){
+    let queryCreateTask = 'INSERT INTO tasks (name, description, dueDate) \
+    VALUES("'+req.body.name+'","'+req.body.description+'","'+req.body.dueDate+'")';
+    
+    connection.query(queryCreateTask, function(err, results, filds){
+        if(err){
+            res.status(500).json(err);
+        }else{
+          res.status(200).json(results);
+        }
+      })
+}else{
+    
+}
    
 })
 
 router.delete('/removeTask/:id', function(req, res, next){
-    // aca se agrega un indice usando la fecha actual y un numero random
-    let timestamp = Date.now() + Math.random();
-    // evaluamos que exita el body y los parametos que definimos
     if(req.params && req.params.id){
-        // pedimos que revise que hay parametros y que se envie el id
         let id = req.params.id;
-        tasks = tasks.filter(task=> task.id !== id)// recorre el arreglo y si encuentra un id igual al que enviamos
-        res.status(200).json(tasks);//aca agregamos el 200 porque todo esta correcto
+        let queryDeleteTask='DELETE FROM tasks WHERE id="'+id+'"';
+        connection.query(queryDeleteTask, function(err, results, filds){
+            if(err){
+                res.status(500).json(err);
+            }else{
+              res.status(200).json(results);
+            }
+          })
+
     } else{
-        res.status(400).json([{}]);//estado 400 evaluando que no se enviaron todos los parametrso  
+        res.status(400).json([{}]);
     }
-   
 })
 module.exports = router;
